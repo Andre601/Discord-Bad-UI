730d611e1a000cb378e4290acf76c56051dc8cf1
Andre601 <Andre601@users.noreply.github.com>
2023-09-10 19:46:01 +0000

    deploy: 6d67e786f65a2fe2b487b671fcc6c9a01787e697

--- assets/js/randomized_subtitle.js
+++ assets/js/randomized_subtitle.js
@@ -3,26 +3,18 @@ const subtitlesCollection = {
     1: "...because there isn't enough nightmare fuel already.",
     2: "...as long as you don't compare it to any other site.",
     3: "...because Electron is the best software you can use, right?",
-}
+};
 
-function setSubtitle() {
-    const numSubtitles = Object.keys(subtitlesCollection).length;
-    const randomNum = Math.random(Math.random * numSubtitles);
+document$.subscribe(() => {
+    const subListSize = Object.keys(subtitlesCollection).length;
+    const randomNum = Math.floor(Math.random() * subListSize);
     
-    const subtitles = document.querySelectorAll('h2[data-js="subtitle"]');
+    const subtitles = document.querySelectorAll('h2[data-md-component="random-subtitle"]');
     subtitles.forEach((subtitle) => {
+        console.log(subtitlesCollection);
+        console.log(randomNum);
+        console.log(subtitlesCollection[randomNum]);
+        
         subtitle.innerText = subtitlesCollection[randomNum];
-    });
-}
-
-function onLoad() {
-    setSubtitle();
-    
-    document.removeEventListener('DOMContentLoaded', onload);
-}
-
-if(document.readyState != 'loading') {
-    onLoad();
-} else {
-    document.addEventListener('DOMContentLoaded', onload);
-}
+    })
+})
